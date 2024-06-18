a60aa554a03fcd326a550e549d3712098fc7a495
Nicolas Cardenas <nico.cardenas260494@gmail.com>
2024-06-18 18:48:43 -0300

    modificacion de personajes

--- app/main.js
+++ app/main.js
@@ -1,8 +1,17 @@
 const $divCards = document.querySelector(".grid");
-const url = "https://hp-api.onrender.com/api/characters";
 
 fetch("https://hp-api.onrender.com/api/characters")
   .then((resp) => resp.json())
   .then((data) => {
-    console.log(data);
+    const personajes = data;
+    console.log(personajes);
+
+    personajes.forEach((personaje) => {
+      $divCards.innerHTML += `<h4>${personaje.name}</h4>
+      <img src="${personaje.image}" alt="imagen de ${personaje.name}">
+       <p>Actor: ${personaje.actor}</p>
+        <p>Gender: ${personaje.gender}</p>
+         <p>House: ${personaje.house}</p>
+          <p>Alive: ${personaje.alive}</p>`;
+    });
   });
