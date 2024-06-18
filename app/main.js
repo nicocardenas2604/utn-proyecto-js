a201a9d43f4e0cea8ac72b1963a6dbbd48c6acff
Nicolas Cardenas <nico.cardenas260494@gmail.com>
2024-06-18 19:30:58 -0300

    modificacion de status y genero

--- app/main.js
+++ app/main.js
@@ -3,15 +3,31 @@ const $divCards = document.querySelector(".grid");
 fetch("https://hp-api.onrender.com/api/characters")
   .then((resp) => resp.json())
   .then((data) => {
-    const personajes = data;
+    const personajes = data.slice(0, 30);
     console.log(personajes);
 
     personajes.forEach((personaje) => {
+      let genero = personaje.gender;
+      if (genero === "male") {
+        genero = "masculino";
+      } else if (genero === "female") {
+        genero = "femenino";
+      } else {
+        genero = "desconocido";
+      }
+
+      let estado = personaje.alive;
+      if (estado) {
+        estado = "vivo";
+      } else {
+        estado = "fallecido";
+      }
+
       $divCards.innerHTML += `<div> <h4>${personaje.name}</h4>
       <img src="${personaje.image}" alt="imagen de ${personaje.name}">
        <p>Actor: ${personaje.actor}</p>
-        <p>Genero: ${personaje.gender}</p>
+        <p>Genero: ${genero}</p>
          <p>Casa: ${personaje.house}</p>
-          <p>Estado: ${personaje.alive}</p> </div>`;
+          <p>Estado: ${estado}</p> </div>`;
     });
   });
